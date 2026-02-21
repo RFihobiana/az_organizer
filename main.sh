5c1872b504640dc0b091b688783c70d55d0a087a
RFihobiana <RaFihobiananyAvo@gmail.com>
2026-02-21 16:19:15 +0300

    Explain what the program is doing

---
+++ main.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+###################################################
+# AZ-ORGANIZER is a program that organize files
+# in a folder.
+# 
+# HOW IT WORK:
+# It just create a folder: its name is the first
+# character of the filename. Then move that file in
+###################################################
+
