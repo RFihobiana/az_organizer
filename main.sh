cd92109520d674ddb876bfb6ae2a455b23bc3b2c
RFihobiana <RaFihobiananyAvo@gmail.com>
2026-02-21 17:01:12 +0300

    Move automatically files into their organized folder

--- main.sh
+++ main.sh
@@ -9,3 +9,16 @@
 # character of the filename. Then move that file in
 ###################################################
 
+for filename in *
+do
+    # Don't recreate again an already aranged file
+    [ "${#filename}" -eq 1 ] && continue
+
+    corresponding_directory=$(echo "${filename:0:1}" | tr "[:lower:]" "[:upper:]")
+
+    echo "$filename -> $corresponding_directory"
+
+    # Execute moving
+    mkdir -p "$corresponding_directory"
+    mv "$filename" "$corresponding_directory"
+done
